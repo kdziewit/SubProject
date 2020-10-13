7108cf9eb27a93ad01ccad2bbce92ff0e841b740
Krzysztof Dziewit <k.dziewit@predictivesolutions.pl>
2020-10-13 13:25:15 +0200

    Squashed 'sub/' changes from a317ec8..7f68c42
    7f68c42 zmina w obu projektach
    1e99f24 zmiana w sub
    
    git-subtree-dir: sub
    git-subtree-split: 7f68c42749cfe3ad87dceb53b85c9c53b070af74

--- SubProject/Test.cs
+++ SubProject/Test.cs
@@ -1,9 +1,11 @@
 ﻿using System;
-
+//zmiana w obu projektach w jednym commit'cie
 namespace SubProject
 {
     public class Test
     {
+
+        //zmiana w sub
         public void Run()
         {
             Console.WriteLine("SubProject");
